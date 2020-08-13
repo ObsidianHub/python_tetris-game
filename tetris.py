fbe3bb3c2bef23dfcf1c23beb57e118b59b35361
Denny <obsidian.y.dragon@gmail.com>
2020-08-13 23:00:00 +0300

    create global main variables

--- tetris.py
+++ tetris.py
@@ -3,3 +3,10 @@ import random
 
 pygame.font.init()
 
+# GLOBAL VARS
+s_width = 800
+s_height = 700
+play_width = 300  # meaning 300 // 10 = 30 width per block
+play_height = 600 # meaning 600 // 20 = 30 height per block
+block_size = 30
+
